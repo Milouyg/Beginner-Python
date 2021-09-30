05a3a28be1493fc5287faa46e0aa2ef2780308e2
Milouyg <33333@ma-web.nl>
2021-09-30 13:08:19 +0200

    Gedichtje

---
+++ Gedicht.py
@@ -0,0 +1,25 @@
+#Gedicht over mezelf
+
+from datetime import datetime
+now = datetime.now()
+print(now)
+
+a = """
+Hey jij daar! 
+Ik ben Milou en ik zit in het eerste jaar. 
+Ik volg de opleiding 'Software Developer' niveau 4 op het Ma 
+Daar doen we veel met elektronica 
+Den Helder.. het noordelijkste puntje waar ik woon. 
+Daar zit ik vaak achter met mijn pc, waar ik me vertoon
+In mijn vrije tijd zit ik vaak achter Discord en speel ik vaak Valorant
+In de klas speelde mensen amper dit en was dit eerst niet zo bekent
+Ook vind ik het erg leuk om met vrienden af te spreken 
+Door mijn fantastische klas heb ik ook erg veel zin in deze schoolweken
+Ik ben ook erg blij dat er geen mensen bijzitten die ik niet mag 
+Want ik kan niet goed opschieten met dat egoïstische, opschepperige en asociaalgedrag
+Ik denk dat je nu wel ongeveer een idee hebt van wie ik ben
+En wie ben jij? Misschien dat ik je ken\n
+"""
+
+print(a)
+
